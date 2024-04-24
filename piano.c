57cbac830ff4959496e2add6ad2357c46d81cbe6
Darkkid819 <Darkkid8192004@gmail.com>
2024-04-24 19:59:18 -0400

    added sfx to notes

--- piano.c
+++ piano.c
@@ -3,7 +3,9 @@
 #include <stdbool.h>
 
 static void DrawPianoKeys(int, int);
-static void PlayNote(char, bool);
+static void PlayNote(char, bool, bool);
+
+bool keyPressedState[256] = { false };
 
 fluid_settings_t* settings = NULL;
 fluid_synth_t* synth = NULL;
@@ -18,11 +20,26 @@ int main() {
     synth = new_fluid_synth(settings);
     adriver = new_fluid_audio_driver(settings, synth);
 
+    fluid_synth_sfload(synth, "resources/piano.sf2", 1);
+
     InitWindow(screenWidth, screenHeight, "Piano");
 
     SetTargetFPS(60); 
 
     while (!WindowShouldClose()) {
+        PlayNote('S', IsKeyDown(KEY_S), keyPressedState['S']); // C4
+        PlayNote('E', IsKeyDown(KEY_E), keyPressedState['E']); // C#4/Db4
+        PlayNote('D', IsKeyDown(KEY_D), keyPressedState['D']); // D4
+        PlayNote('R', IsKeyDown(KEY_R), keyPressedState['R']); // D#4/Eb4
+        PlayNote('F', IsKeyDown(KEY_F), keyPressedState['F']); // E4
+        PlayNote('J', IsKeyDown(KEY_J), keyPressedState['J']); // F4
+        PlayNote('I', IsKeyDown(KEY_I), keyPressedState['I']); // F#4/Gb4
+        PlayNote('K', IsKeyDown(KEY_K), keyPressedState['K']); // G4
+        PlayNote('O', IsKeyDown(KEY_O), keyPressedState['O']); // G#4/Ab4
+        PlayNote('L', IsKeyDown(KEY_L), keyPressedState['L']); // A4
+        PlayNote('P', IsKeyDown(KEY_P), keyPressedState['P']); // A#4/Bb4
+        PlayNote(';', IsKeyDown(KEY_SEMICOLON), keyPressedState[';']); // B4
+
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
@@ -78,6 +95,30 @@ static void DrawPianoKeys(int screenWidth, int screenHeight) {
     }
 }
 
-static void PlayNote(char, bool) {
-    
+static void PlayNote(char key, bool pressed, bool keyAlreadyPressed) {
+    int note = -1;
+    switch (key) {
+        case 'S': note = 60; break; // C4
+        case 'E': note = 61; break; // C#4/Db4
+        case 'D': note = 62; break; // D4
+        case 'R': note = 63; break; // D#4/Eb4
+        case 'F': note = 64; break; // E4
+        case 'J': note = 65; break; // F4
+        case 'I': note = 66; break; // F#4/Gb4
+        case 'K': note = 67; break; // G4
+        case 'O': note = 68; break; // G#4/Ab4
+        case 'L': note = 69; break; // A4
+        case 'P': note = 70; break; // A#4/Bb4
+        case ';': note = 71; break; // B4
+    }
+
+    if (note != -1) {
+        if (pressed && !keyAlreadyPressed) {
+            fluid_synth_noteon(synth, 0, note, 127);
+            keyPressedState[key] = true;
+        } else if (!pressed && keyAlreadyPressed) {
+            fluid_synth_noteoff(synth, 0, note);
+            keyPressedState[key] = false; 
+        }
+    }
 }
